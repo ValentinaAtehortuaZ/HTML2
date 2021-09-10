bb510820d96fbe03ba7f47f3c99821b8d27d5b95
ValentinaAtehortuaZ <valentinaatehortuaz@gmail.com>
2021-09-10 17:20:22 -0500

    xx

---
+++ Assets/javascript.js
@@ -0,0 +1,15 @@
+var slideIndex = 1;
+
+
+function plusSlides(n) {
+
+}
+
+function showSlides(n) {
+    var slides = document.getElementsByClassName("mySlide");
+    var dots = document.getElementsByClassName("dot");
+    
+
+
+    
+}

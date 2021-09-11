fa8934a90a6adf0fc85a4085fe6ad824db42a2c1
ValentinaAtehortuaZ <valentinaatehortuaz@gmail.com>
2021-09-10 19:16:40 -0500

    xx

--- Assets/javascript.js
+++ Assets/javascript.js
@@ -12,26 +12,31 @@ function currentSlide(n) {
 function showSlides(n) {
     var slides = document.getElementsByClassName("mySlide");
     var dots = document.getElementsByClassName("dot");
-    
+
 
     if (n > slides.length) {
-        slideIndex = 1;  
+        slideIndex = 1;
     }
 
     if (n < 1) {
-        slideIndex = slides.length;  
+        slideIndex = slides.length;
     }
 
-for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-}
-for (let i = 0; i < dots.length; i++) {
-    dots[i].className =  dots[i].className.replace(" active", "");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+
+    slideIndex++;
+
+    setTimeout(() => {
+        showSlides(slideIndex)
+    }, 10000);
+
 }
 
-slides[slideIndex-1].style.display = "block";
-dots[slideIndex-1].className += "active";
-
-
-}
-     
